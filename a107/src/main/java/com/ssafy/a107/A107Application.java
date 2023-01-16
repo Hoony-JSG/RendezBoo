a4e0f056405f4ef52fb812d1398a69cc749ae4e4
Bluuubery <zephyr72836@gmail.com>
2023-01-16 10:55:51 +0900

    feat: Initialize pjt

---
+++ a107/src/main/java/com/ssafy/a107/A107Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.a107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A107Application.class, args);
+	}
+
+}
