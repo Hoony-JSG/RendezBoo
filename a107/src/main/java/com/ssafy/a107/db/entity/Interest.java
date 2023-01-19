b683b0415bb390350915a8ede4c306e7f0afcad6
ljaewon97 <ljaewon97@gmail.com>
2023-01-19 14:35:24 +0900

    fix: Setter 삭제

--- a107/src/main/java/com/ssafy/a107/db/entity/Interest.java
+++ a107/src/main/java/com/ssafy/a107/db/entity/Interest.java
@@ -1,13 +1,12 @@
 package com.ssafy.a107.db.entity;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
-@Getter @Setter
+@Getter
 public class Interest extends BaseEntity {
 
     @Column(nullable = false, length = 20)
