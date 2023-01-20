2139289f5b1b41a89f0c136c1cf384d2333d0b8c
ljaewon97 <ljaewon97@gmail.com>
2023-01-20 11:37:25 +0900

    feat: UserController에 getUserInfo 추가

--- a107/src/main/java/com/ssafy/a107/api/service/UserServiceImpl.java
+++ a107/src/main/java/com/ssafy/a107/api/service/UserServiceImpl.java
@@ -1,7 +1,14 @@
 package com.ssafy.a107.api.service;
 
+import com.ssafy.a107.db.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    
+    @Override
+    public User getUserBySeq(Long userSeq) {
+        // TODO: JPA 뉴비 이슈로 보류
+        return null;
+    }
 }
