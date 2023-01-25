d4b4a2a51ff800a106216bef96ad82ba1c637bec
developerhongjulee <developerhongjulee@gmail.com>
2023-01-25 15:55:25 +0900

    fix: ReportService를 ReportService interface와 ReportServiceImpl 클래스로 분리

--- a107/src/main/java/com/ssafy/a107/api/service/ReportService.java
+++ a107/src/main/java/com/ssafy/a107/api/service/ReportService.java
@@ -2,39 +2,10 @@ package com.ssafy.a107.api.service;
 
 import com.ssafy.a107.api.request.ReportReq;
 import com.ssafy.a107.api.response.ReportRes;
-import com.ssafy.a107.db.entity.Report;
-import com.ssafy.a107.db.entity.User;
-import com.ssafy.a107.db.repository.ReportRepository;
-import com.ssafy.a107.db.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@RequiredArgsConstructor
-@Service
-public class ReportService {
-
-    private final ReportRepository reportRepository;
-    private final UserRepository userRepository;
-    @Transactional
-    public Long save(ReportReq req){
-        User reporter = userRepository.findBySeq(req.getReporterSeq());
-        User target = userRepository.findBySeq(req.getTargetSeq());
-        Report newReport = Report.builder()
-                .reporter(reporter)
-                .target(target)
-                .reportType(req.getReportType())
-                .reportDetail(req.getReportDetail())
-                .build();
-        return reportRepository.save(newReport).getSeq();
-    }
-    public List<ReportRes> findAll(){
-        List<Report> entityAll = reportRepository.findAll();
-        List<ReportRes> resAll = new ArrayList<>();
-        for(Report r: entityAll) resAll.add(new ReportRes(r));
-        return resAll;
-    }
+public interface ReportService {
+    Long save(ReportReq req);
+    List<ReportRes> findAll();
 }
