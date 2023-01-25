2afaefab9e97820961aba26b723df8d6bea9ff72
developerhongjulee <developerhongjulee@gmail.com>
2023-01-25 15:04:15 +0900

    fix: ReportController클래스 내에서 service를 생성자로 bean등록하도록 수정

--- a107/src/main/java/com/ssafy/a107/api/controller/ReportController.java
+++ a107/src/main/java/com/ssafy/a107/api/controller/ReportController.java
@@ -4,7 +4,7 @@ import com.ssafy.a107.api.request.ReportReq;
 import com.ssafy.a107.api.response.ReportRes;
 import com.ssafy.a107.api.service.ReportService;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Api(value = "신고 API", tags = {"Report"})
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/report")
 public class ReportController {
 
-    @Autowired
-    private ReportService reportService;
+    private final ReportService reportService;
 
     @PostMapping("/")
     public ResponseEntity<?> save(@RequestBody ReportReq req){
