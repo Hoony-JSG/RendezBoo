2e8f0dba7588d4b12322248b169040de22bd3717
developerhongjulee <developerhongjulee@gmail.com>
2023-01-25 16:14:40 +0900

    fix: BadgeCreateReq에서 입력값을 이용한 constructor생성, 해당 생성자를 이용한 빌더 생성, AllArgsConstructor삭제, 클래스 빌더 삭제

--- a107/src/main/java/com/ssafy/a107/api/request/BadgeCreateReq.java
+++ a107/src/main/java/com/ssafy/a107/api/request/BadgeCreateReq.java
@@ -1,15 +1,18 @@
 package com.ssafy.a107.api.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class BadgeCreateReq {
     private String name;
     private String url;
+
+    @Builder
+    public BadgeCreateReq(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 }
