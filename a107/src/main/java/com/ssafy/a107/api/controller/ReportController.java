a07fcd6d38452f84eb6577c84c884052d4223de2
developerhongjulee <developerhongjulee@gmail.com>
2023-01-25 15:01:52 +0900

    fix: (중요!!)req, res가 service단에서 처리되도록 변경!!(Controller클래스)

--- a107/src/main/java/com/ssafy/a107/api/controller/ReportController.java
+++ a107/src/main/java/com/ssafy/a107/api/controller/ReportController.java
@@ -1,53 +1,33 @@
 package com.ssafy.a107.api.controller;
 
 import com.ssafy.a107.api.request.ReportReq;
+import com.ssafy.a107.api.response.ReportRes;
 import com.ssafy.a107.api.service.ReportService;
-import com.ssafy.a107.api.service.UserService;
-import com.ssafy.a107.db.entity.Report;
-import com.ssafy.a107.db.entity.User;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "신고 API", tags = {"Report"})
 @RestController
+@RequestMapping("/api/report")
 public class ReportController {
 
     @Autowired
     private ReportService reportService;
 
-    @Autowired
-    private UserService userService;
-
-    @PostMapping("/api/report")
+    @PostMapping("/")
     public ResponseEntity<?> save(@RequestBody ReportReq req){
-        Long reporterSeq = req.getReporterSeq();
-        Long targetSeq = req.getTargetSeq();
-
-        User reporter = userService.getUserBySeq(reporterSeq);
-        User target = userService.getUserBySeq(targetSeq);
-
-        Report report = Report.builder()
-                .reporter(reporter)
-                .target(target)
-                .reportType(req.getReportType())
-                .reportDetail(req.getReportDetail())
-                .reportDetail(req.getReportDetail())
-                .build();
-
-        Long reportSeq = reportService.save(report);
-
+        Long reportSeq = reportService.save(req);
         return ResponseEntity.status(HttpStatus.CREATED).body(reportSeq);
     }
 
-    @GetMapping("/api/report")
+    @GetMapping("/")
     public ResponseEntity<?> findAll(){
-        List<Report> all = reportService.findAll();
+        List<ReportRes> all = reportService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(all);
     }
 }
