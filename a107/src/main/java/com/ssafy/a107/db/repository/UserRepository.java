df96c2e677cb1ce6cec6af803c04e3522b91e5d3
ljaewon97 <ljaewon97@gmail.com>
2023-01-25 14:20:37 +0900

    feat: 회원가입 및 테스트 구현

--- a107/src/main/java/com/ssafy/a107/db/repository/UserRepository.java
+++ a107/src/main/java/com/ssafy/a107/db/repository/UserRepository.java
@@ -1,16 +1,15 @@
 package com.ssafy.a107.db.repository;
 
 import com.ssafy.a107.db.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
-
-    public Optional<User> findByEmail(String email);
-
+    User findByEmail(String email);
     User findBySeq (Long seq);
 
 
