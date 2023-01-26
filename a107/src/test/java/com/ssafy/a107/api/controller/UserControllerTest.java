61326c305f210fc6726f940fcdd39ce843270708
ljaewon97 <ljaewon97@gmail.com>
2023-01-26 16:59:28 +0900

    test: 이메일 중복 체크 테스트 성공

--- a107/src/test/java/com/ssafy/a107/api/controller/UserControllerTest.java
+++ a107/src/test/java/com/ssafy/a107/api/controller/UserControllerTest.java
@@ -4,22 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.a107.api.request.JoinReq;
 import com.ssafy.a107.api.request.LoginReq;
 import com.ssafy.a107.api.service.UserService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -94,5 +89,53 @@ class UserControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void checkEmailConflictTest() throws Exception {
+        //given
+        JoinReq joinReq = JoinReq.builder()
+                .email("test@ssafy.com")
+                .password("test123")
+                .city("Seoul")
+                .gender(true)
+                .phoneNumber("010-1234-1234")
+                .name("김싸피")
+                .profileImagePath("test.jpg")
+                .MBTI("INTP")
+                .build();
+
+        //when
+        userService.createUser(joinReq);
+
+        //then
+        mvc.perform(MockMvcRequestBuilders.get("/api/user/check/test@ssafy.com")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void checkEmailOKTest() throws Exception {
+        //given
+        JoinReq joinReq = JoinReq.builder()
+                .email("test@ssafy.com")
+                .password("test123")
+                .city("Seoul")
+                .gender(true)
+                .phoneNumber("010-1234-1234")
+                .name("김싸피")
+                .profileImagePath("test.jpg")
+                .MBTI("INTP")
+                .build();
+
+        //when
+        userService.createUser(joinReq);
+
+        //then
+        mvc.perform(MockMvcRequestBuilders.get("/api/user/check/ssafykim@ssafy.com")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
     }
 }
