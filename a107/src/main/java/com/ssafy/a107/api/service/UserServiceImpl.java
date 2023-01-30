18e72a852fdde176bfc57f492d26339fec91ef7a
ljaewon97 <ljaewon97@gmail.com>
2023-01-30 17:27:26 +0900

    feat: Kakao OAuth 구현

--- a107/src/main/java/com/ssafy/a107/api/service/UserServiceImpl.java
+++ a107/src/main/java/com/ssafy/a107/api/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.ssafy.a107.common.exception.NotFoundException;
 import com.ssafy.a107.db.entity.User;
 import com.ssafy.a107.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -19,12 +20,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
     
     private final UserRepository userRepository;
@@ -135,5 +140,85 @@ public class UserServiceImpl implements UserService {
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         return new HttpEntity<>(headers);
+    }
+
+    @Override
+    public String getKakaoAccessToken(String code) {
+        String accessToken = "";
+        String tokenUrl = "https://kauth.kakao.com/oauth/token";
+
+        try {
+            URL url = new URL(tokenUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true); // POST
+
+            // 쿼리 스트링
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=712c281c808f36c63bc8cea4a11ba42f");
+            sb.append("&client_secret=DGuf0uZuIjHSWb144JPJNsPBfxsSKJVD");
+            sb.append("&redirect_uri=http://localhost:8080/api/oauth/kakao");
+            sb.append("&code=" + code);
+            bw.write(sb.toString());
+            bw.flush();
+
+            int responseCode = conn.getResponseCode();
+            log.debug("Kakao OAuth responseCode: {}", responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder res = new StringBuilder();
+            String line = "";
+
+            while((line = br.readLine()) != null) {
+                res.append(line);
+            }
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(res.toString());
+
+            accessToken = element.getAsJsonObject().get("access_token").getAsString();
+
+            br.close();
+            bw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return accessToken;
+    }
+
+    @Override
+    public String getKakaoProfile(String token) throws Exception {
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        StringBuilder res = new StringBuilder();
+        String line = "";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + token);
+
+            if(conn.getResponseCode() == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                while((line = br.readLine()) != null) {
+                    res.append(line);
+                }
+
+                br.close();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return res.toString();
     }
 }
