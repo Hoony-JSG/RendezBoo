b5ed703dde3ac9b94bf018a8fe3e0fd52fe31773
Bluuubery <zephyr72836@gmail.com>
2023-01-30 13:46:50 +0900

    Feat: 방번호로 찾기, seq로 찾기 추가

---
+++ a107/src/main/java/com/ssafy/a107/api/service/ChatService.java
@@ -0,0 +1,17 @@
+package com.ssafy.a107.api.service;
+
+import com.ssafy.a107.api.request.ChatReq;
+import com.ssafy.a107.common.exception.NotFoundException;
+import com.ssafy.a107.db.entity.Chat;
+
+import java.util.List;
+
+public interface ChatService {
+
+    String insertChat(ChatReq chatReq) throws NotFoundException;
+
+    Chat findBySeq(String seq) throws NotFoundException;
+
+    List<Chat> findByChatRoomSeqOrderByCreatedAtDesc(String chatRoomSeq) throws NotFoundException;
+
+}
--- a107/src/main/java/com/ssafy/a107/db/repository/ChatRepository.java
+++ a107/src/main/java/com/ssafy/a107/db/repository/ChatRepository.java
@@ -3,7 +3,14 @@ package com.ssafy.a107.db.repository;
 import com.ssafy.a107.db.entity.Chat;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends MongoRepository <Chat, Long> {
 
     Chat findTopByOrderByCreatedAtDesc();
+    
+//    채팅방 번호로 찾아서 생성일 내림차순으로 반환
+    List<Chat> findByChatRoomSeqOrderByCreatedAtDesc(String chatRoomSeq);
+
+    Chat findBySeq(String seq);
 }
