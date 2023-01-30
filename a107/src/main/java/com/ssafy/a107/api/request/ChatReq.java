d196f6ceea01842b98ffe5df3fd4e0ccc079e024
Bluuubery <zephyr72836@gmail.com>
2023-01-30 13:47:35 +0900

    Feat: chat request dto 생성

---
+++ a107/src/main/java/com/ssafy/a107/api/request/ChatReq.java
@@ -0,0 +1,18 @@
+package com.ssafy.a107.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatReq {
+
+    private String message;
+    private Long chatRoomSeq;
+    private Long senderSeq;
+    private Long receiverSeq;
+}
