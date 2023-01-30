fa2eb6acefc552e889332331e40629ddf192e2e8
Bluuubery <zephyr72836@gmail.com>
2023-01-30 13:47:11 +0900

    Feat: auditing 추가

--- a107/src/main/java/com/ssafy/a107/config/MongoDbConfig.java
+++ a107/src/main/java/com/ssafy/a107/config/MongoDbConfig.java
@@ -1,7 +1,11 @@
 package com.ssafy.a107.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
+@EnableMongoAuditing
 public class MongoDbConfig {
+
+
 }
