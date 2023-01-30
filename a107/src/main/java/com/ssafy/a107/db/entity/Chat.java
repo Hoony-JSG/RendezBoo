47ef5f149a220bbab560a1fb7a106471c32f80f3
Bluuubery <zephyr72836@gmail.com>
2023-01-30 10:05:08 +0900

    Feat: chat entity id, builder 생성

--- a107/src/main/java/com/ssafy/a107/db/entity/Chat.java
+++ a107/src/main/java/com/ssafy/a107/db/entity/Chat.java
@@ -1,8 +1,10 @@
 package com.ssafy.a107.db.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,12 +15,21 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Chat extends BaseEntity{
+public class Chat{
 
+    @Id
+    private Long seq;
     private String message;
     private Long senderSeq;
     private Long receiverSeq;
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @Builder
+    public Chat(String message, Long senderSeq, Long receiverSeq) {
+        this.message = message;
+        this.senderSeq = senderSeq;
+        this.receiverSeq = receiverSeq;
+    }
 }
