9d65bff00dd59536e84df80febe7ddf1c81ed7dc
developerhongjulee <developerhongjulee@gmail.com>
2023-01-31 15:44:09 +0900

    feat: MultyMeetingRoom으로 엔티티명 변경, MultyMeetingRoom에 포함된 사람들 엔티티로 따로 뺄 예정

--- a107/src/main/java/com/ssafy/a107/api/request/EmotionDataReq.java
+++ a107/src/main/java/com/ssafy/a107/api/request/EmotionDataReq.java
@@ -1,11 +1,14 @@
 package com.ssafy.a107.api.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EmotionDataReq {
     private Double anger;
     private Double contempt;
@@ -18,17 +21,4 @@ public class EmotionDataReq {
 
     private Long meeting_room_seq;
     private Long user_seq;
-    @Builder
-    public EmotionDataReq(Double anger, Double contempt, Double disgust, Double fear, Double happiness, Double neutral, Double sadness, Double surprise, Long meeting_room_seq, Long user_seq) {
-        this.anger = anger;
-        this.contempt = contempt;
-        this.disgust = disgust;
-        this.fear = fear;
-        this.happiness = happiness;
-        this.neutral = neutral;
-        this.sadness = sadness;
-        this.surprise = surprise;
-        this.meeting_room_seq = meeting_room_seq;
-        this.user_seq = user_seq;
-    }
 }
