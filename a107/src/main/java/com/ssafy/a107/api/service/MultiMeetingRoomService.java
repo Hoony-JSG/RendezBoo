500233e19a740c98461347ea89b48a77fb83e8e7
developerhongjulee <developerhongjulee@gmail.com>
2023-01-31 17:37:49 +0900

    feat: MultiMeetingRoomService 일부 구현

--- a107/src/main/java/com/ssafy/a107/api/service/MultiMeetingRoomService.java
+++ a107/src/main/java/com/ssafy/a107/api/service/MultiMeetingRoomService.java
@@ -4,10 +4,12 @@ package com.ssafy.a107.api.service;
 import com.ssafy.a107.api.request.MultyMeetingRoomReq;
 import com.ssafy.a107.api.response.MultiMeetingRoomRes;
 import com.ssafy.a107.common.exception.NotFoundException;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
 
 public interface MultiMeetingRoomService {
 
-    Long saveMultiMeetingRoom(MultyMeetingRoomReq multyMeetingRoomReq);
+    Long saveMultiMeetingRoom(MultyMeetingRoomReq multyMeetingRoomReq) throws NotFoundException, OpenViduJavaClientException, OpenViduHttpException;
     MultiMeetingRoomRes getMultiMeetingRoom(Long roomSeq) throws NotFoundException;
 
     Long joinMultiMeetingRoom(Long meetingRoomSeq, Long userSeq) throws NotFoundException;
--- a107/src/main/java/com/ssafy/a107/api/service/MultiMeetingRoomServiceImpl.java
+++ a107/src/main/java/com/ssafy/a107/api/service/MultiMeetingRoomServiceImpl.java
@@ -3,16 +3,21 @@ package com.ssafy.a107.api.service;
 import com.ssafy.a107.api.request.MultyMeetingRoomReq;
 import com.ssafy.a107.api.response.MultiMeetingRoomRes;
 import com.ssafy.a107.common.exception.NotFoundException;
+import com.ssafy.a107.common.util.SessionKeyProvider;
+import com.ssafy.a107.db.entity.MultiMeetingRoom;
+import com.ssafy.a107.db.entity.MultiMeetingRoomUser;
+import com.ssafy.a107.db.entity.User;
 import com.ssafy.a107.db.repository.MultiMeetingRoomUserRepository;
 import com.ssafy.a107.db.repository.MultiMeetingRoomRepository;
 import com.ssafy.a107.db.repository.UserRepository;
-import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -38,14 +43,25 @@ public class MultiMeetingRoomServiceImpl implements MultiMeetingRoomService {
 
 
     @Override
-    public Long saveMultiMeetingRoom(MultyMeetingRoomReq multyMeetingRoomReq) {
+    public Long saveMultiMeetingRoom(MultyMeetingRoomReq multyMeetingRoomReq) throws NotFoundException, OpenViduJavaClientException, OpenViduHttpException {
+        User user = userRepository.findById(multyMeetingRoomReq.getUserSeq())
+                .orElseThrow(()->new NotFoundException("Invalid User sequence!"));
 
+        Map<String, Object> params = Map.of("customSessionId", SessionKeyProvider.getSessionKey(false));
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openVidu.createSession(properties);
+        MultiMeetingRoom multiMeetingRoom = MultiMeetingRoom.builder()
+                .title(multyMeetingRoomReq.getTitle())
+                .status((byte)0).build();
+        multiMeetingRoom.createSession(session.getSessionId());
 
+        multiMeetingRoomUserRepository.save(new MultiMeetingRoomUser(user, multiMeetingRoom));
         return null;
     }
     @Override
     public MultiMeetingRoomRes getMultiMeetingRoom(Long roomSeq) throws NotFoundException {
         int maleNum=0, femaleNum=0;
+        //
         return new MultiMeetingRoomRes(multyMeetingRoomRepository.findById(roomSeq)
                 .orElseThrow(()->new NotFoundException("Invalid multiMeetingRoom sequence")),
                 maleNum, femaleNum
