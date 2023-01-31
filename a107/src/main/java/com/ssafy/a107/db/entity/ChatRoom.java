aa64809880a2ea7ab6483e5abe18c5cf34d7dc22
Bluuubery <zephyr72836@gmail.com>
2023-01-31 11:24:30 +0900

    Feat: 채팅방 엔티티 생성

---
+++ a107/src/main/java/com/ssafy/a107/db/entity/ChatRoom.java
@@ -0,0 +1,29 @@
+package com.ssafy.a107.db.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ChatRoom extends BaseEntity{
+
+//    남자 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_male_id")
+    private User userMale;
+
+//    여자 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_female_id")
+    private User userFemale;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
