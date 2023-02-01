3ee89636600065874d6158b6754da01638267f8f
Bluuubery <zephyr72836@gmail.com>
2023-02-01 17:25:32 +0900

    Fix: 웹소켓 config cors 설정 변경, stomp endpoint 주소 이름 변경

--- a107/src/main/java/com/ssafy/a107/config/WebSocketConfig.java
+++ a107/src/main/java/com/ssafy/a107/config/WebSocketConfig.java
@@ -2,9 +2,7 @@ package com.ssafy.a107.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -14,15 +12,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOrigins("*") // 임시로 다 열어둠
+                .setAllowedOriginPatterns("*")
+//                .setAllowedOrigins("*") // 임시로 다 열어둠
                 .withSockJS();
     }
 
 //    다른 stomp 연결과 구분 지을 수도 있어서 일단 chat으로 이름 지음
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/receive-chat");
-        registry.setApplicationDestinationPrefixes("/send-chat");
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 
 
