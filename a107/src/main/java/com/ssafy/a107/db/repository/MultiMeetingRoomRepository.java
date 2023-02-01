95680611b55749474aa69d38dc7cb2831db9c53e
developerhongjulee <developerhongjulee@gmail.com>
2023-02-01 16:07:58 +0900

    feat: MultiMeetingRoomRepository에 findUsersByMultiMeetingRoomSeq 메서드 추가(user.isValid 고려해서 추후 수정해야 함)

--- a107/src/main/java/com/ssafy/a107/db/repository/MultiMeetingRoomRepository.java
+++ a107/src/main/java/com/ssafy/a107/db/repository/MultiMeetingRoomRepository.java
@@ -1,8 +1,14 @@
 package com.ssafy.a107.db.repository;
 
 import com.ssafy.a107.db.entity.MultiMeetingRoom;
+import com.ssafy.a107.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MultiMeetingRoomRepository extends JpaRepository<MultiMeetingRoom, Long> {
 
+    @Query("select u from MultiMeetingRoomUser mu inner join User u on mu.user.seq = u.seq where mu.multiMeetingRoom.seq = ?1")
+    List<User> findUsersByMultiMeetingRoomSeq(Long roomSeq);
 }
