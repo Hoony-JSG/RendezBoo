37746525e4c78b2af855cde7607a0b6dac379eac
developerhongjulee <developerhongjulee@gmail.com>
2023-02-01 15:02:17 +0900

    feat: MultiMeetingRoom 생성, 조회,전체조회,유저참여,유저나가기(쿼리 작성을 덜 했기 때문에 미팅방 정보 조회의 경우 남,여 인원수가 모두 0으로 나옴-수정예정)

--- a107/src/main/java/com/ssafy/a107/api/controller/MultiMeetingRoomController.java
+++ a107/src/main/java/com/ssafy/a107/api/controller/MultiMeetingRoomController.java
@@ -22,7 +22,7 @@ public class MultiMeetingRoomController {
 
     @ApiOperation(value = "새 단체 미팅방 생성하기",notes = "새 단체 미팅방 생성하기")
     @PostMapping("/")
-    public ResponseEntity<?> makeMultiRoom(MultiMeetingRoomReq multiMeetingRoomReq) throws NotFoundException, OpenViduJavaClientException, OpenViduHttpException {
+    public ResponseEntity<?> makeMultiRoom(@RequestBody MultiMeetingRoomReq multiMeetingRoomReq) throws NotFoundException, OpenViduJavaClientException, OpenViduHttpException {
         Long newRoomSeq = multiMeetingRoomService.saveMultiMeetingRoom(multiMeetingRoomReq);
         return ResponseEntity.status(HttpStatus.CREATED).body(newRoomSeq);
     }
@@ -38,10 +38,10 @@ public class MultiMeetingRoomController {
     public ResponseEntity<?> getAllMultiRoom(){
         return ResponseEntity.status(HttpStatus.OK).body(multiMeetingRoomService.findAllMultiMeetingRoom());
     }
-    @ApiOperation(value = "")
-    @PostMapping("/{meetingRoomSeq}/join/{userSeq}")
-    public ResponseEntity<?> joinMultiRoom(@PathVariable Long meetingRoomSeq, @PathVariable Long userSeq) throws NotFoundException {
-        multiMeetingRoomService.joinMultiMeetingRoom(meetingRoomSeq, userSeq);
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
-    }
+//    @ApiOperation(value = "")
+//    @PostMapping("/{meetingRoomSeq}/join/{userSeq}")
+//    public ResponseEntity<?> joinMultiRoom(@PathVariable Long meetingRoomSeq, @PathVariable Long userSeq) throws NotFoundException {
+//        multiMeetingRoomService.joinMultiMeetingRoom(meetingRoomSeq, userSeq);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+//    }
 }
--- a107/src/main/java/com/ssafy/a107/api/response/MultiMeetingRoomRes.java
+++ a107/src/main/java/com/ssafy/a107/api/response/MultiMeetingRoomRes.java
@@ -2,7 +2,11 @@ package com.ssafy.a107.api.response;
 
 
 import com.ssafy.a107.db.entity.MultiMeetingRoom;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 public class MultiMeetingRoomRes {
     private final String title;
     private final int maleNum;
