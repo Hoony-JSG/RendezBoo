878afe2d2a34585f66f0c0b25d218fbb40c93df6
unknown <hi_cookie@nate.com>
2023-02-02 15:03:00 +0900

    fix: 현재 merge 디테일 수정

--- A107-front/a107-app/src/App.js
+++ A107-front/a107-app/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Rendezboo, Signal, Login, Rocket, Docking1 } from './pages'
+import { Home, Rendezboo, Signal, Login, Rocket, Docking1 } from './pages'
 import Navbar from './components/Navbar';
 import "./App.css";
 import Userinfo from "./pages/Userinfo";
 import Inventory from "./pages/Inventory";
-import Home from "./pages/Home";
 
 function App() {
   return (
@@ -15,8 +14,10 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route exact path="/" element={<Rendezboo />} />
-          <Route path="/signal" element={<Signal />} />
+          <Route exact path="/signal" element={<Signal />} />
+          <Route path="/signal/:userid" element={<Signal />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/docking1" element={<Docking1 />} />
           <Route path="/rocket/:userid" element={<Rocket />} />
           <Route path="/Userinfo/:userid" element={<Userinfo />}></Route>;
           <Route path="/Inventory/:userid" element={<Inventory />}></Route>;
