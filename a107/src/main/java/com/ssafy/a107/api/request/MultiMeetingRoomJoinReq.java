5a76e179a940b3a8d90f36afb5d05ca4a6c9e2dc
developerhongjulee <developerhongjulee@gmail.com>
2023-02-03 13:36:38 +0900

    feat: MultiMeetingRoomJoinReq dto클래스 생성

---
+++ a107/src/main/java/com/ssafy/a107/api/request/MultiMeetingRoomJoinReq.java
@@ -0,0 +1,15 @@
+package com.ssafy.a107.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MultiMeetingRoomJoinReq {
+    String userSeq;
+    Long multiMeetingRoomSeq;
+}
