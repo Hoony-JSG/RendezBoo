6614705c8fa142d7294bf09939c11e33f2aa1abf
ljaewon97 <ljaewon97@gmail.com>
2023-02-03 17:47:15 +0900

    feat: JWT header 관련 enum 추가

---
+++ a107/src/main/java/com/ssafy/a107/common/util/JwtHeaderUtilEnums.java
@@ -0,0 +1,14 @@
+package com.ssafy.a107.common.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum JwtHeaderUtilEnums {
+
+    GRANT_TYPE("JWT 타입 / Bearer ", "Bearer ");
+
+    private String description;
+    private String value;
+}
