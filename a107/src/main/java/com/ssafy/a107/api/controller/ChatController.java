4490783bb571259aa4fe644b65b13df65a611558
Bluuubery <zephyr72836@gmail.com>
2023-02-06 15:59:10 +0900

    Fix: 로그 출력 및 기타 변경
    1. 메세지 송수신 시 로그 출력
    2. 메세지 발송 주소 변경
    3. 매핑 주소 변경
    4. 구현체 -> 인터페이스 의존관계 변경

--- a107/src/main/java/com/ssafy/a107/api/controller/ChatController.java
+++ a107/src/main/java/com/ssafy/a107/api/controller/ChatController.java
@@ -2,7 +2,7 @@ package com.ssafy.a107.api.controller;
 
 import com.ssafy.a107.api.request.ChatReq;
 import com.ssafy.a107.api.response.ChatRes;
-import com.ssafy.a107.api.service.ChatServiceImpl;
+import com.ssafy.a107.api.service.ChatService;
 import com.ssafy.a107.common.exception.NotFoundException;
 import com.ssafy.a107.db.entity.Chat;
 import io.swagger.annotations.Api;
@@ -20,12 +20,12 @@ import java.util.stream.Collectors;
 
 @Api(value = "채팅(친구 추가 이후) API", tags = {"Chat"})
 @RestController
-@RequestMapping("/api/chat")
+//@RequestMapping("/api/chat")
 @RequiredArgsConstructor
 @Slf4j
 public class ChatController {
 
-    private final ChatServiceImpl chatService;
+    private final ChatService chatService;
 
     private final SimpMessageSendingOperations sendingOperations;
 
@@ -35,13 +35,17 @@ public class ChatController {
     @ApiOperation("채팅 생성(보내기) - stomp")
     public ResponseEntity<?> sendChat(@RequestBody ChatReq req) throws NotFoundException{
         log.debug("got message");
+        log.debug("message: " + req.getMessage());
+        log.debug("chatRoomSeq: " + req.getChatRoomSeq());
+        log.debug("senderSeq: " + req.getSenderSeq());
+        log.debug("receiverSeq: " + req.getReceiverSeq());
 
 
         String insertChat = chatService.insertChat(req);
         Chat chat = chatService.findBySeq(insertChat);
 
         ChatRes chatRes = new ChatRes(chat);
-        sendingOperations.convertAndSend("/sub/" + chatRes.getChatRoomSeq(), chatRes);
+        sendingOperations.convertAndSend("/sub/chat/" + chatRes.getChatRoomSeq(), chatRes);
 
 
         return ResponseEntity.status(HttpStatus.CREATED).body(chatRes);
@@ -67,7 +71,7 @@ public class ChatController {
 //        }
 //    }
 
-    @GetMapping("/{chatRoomSeq}")
+    @GetMapping("/api/chat/{chatRoomSeq}")
     @ApiOperation("해당 채팅 방의 채팅 내역 불러오기")
     public ResponseEntity<?> getChatByChatRoomSeq(@PathVariable Long chatRoomSeq) throws NotFoundException{
         List<Chat> chatList = chatService.findByChatRoomSeqOrderByCreatedAtDesc(chatRoomSeq);
