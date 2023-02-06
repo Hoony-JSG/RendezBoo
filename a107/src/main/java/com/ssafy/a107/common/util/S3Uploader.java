3bfc9c3a47b44838181785cbc7f356e1bed712e1
김명준 <audwnswmf@naver.com>
2023-02-06 17:07:58 +0900

    fix: s3 class 이상

--- a107/src/main/java/com/ssafy/a107/common/util/S3Uploader.java
+++ a107/src/main/java/com/ssafy/a107/common/util/S3Uploader.java
@@ -1,8 +1,10 @@
 package com.ssafy.a107.common.util;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.ssafy.a107.config.S3Config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +22,7 @@ import java.util.Optional;
 @Component
 @Service
 public class S3Uploader {
-    private final AmazonS3Client amazonS3Client;
+    private final AmazonS3 amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
