5f6e2cf2672a42795205593ee229c9100109c744
Bluuubery <zephyr72836@gmail.com>
2023-02-08 15:10:11 +0900

    Feat: jwt 테스트용 로그인 페이지 작성

--- A107-front/a107-app/src/App.js
+++ A107-front/a107-app/src/App.js
@@ -17,6 +17,7 @@ import Navbar from './components/Navbar'
 import './App.css'
 import WebSocketChatTest from './modules/WebSocketChatTest'
 import JoinSocial from './pages/JoinSocial'
+import LoginTest from './modules/Auth/testLoginpage'
 
 function App() {
   const location = useLocation()
@@ -50,7 +51,8 @@ function App() {
             path="/websocketchattest"
             element={<WebSocketChatTest />}
           ></Route>
-          ;
+          {/* 로그인 테스트용 라우터 */}
+          <Route path='/logintest' element={<LoginTest />}></Route>
         </Routes>
       </div>
     </div>
---
+++ A107-front/a107-app/src/modules/Auth/testLoginpage.jsx
@@ -0,0 +1,77 @@
+import {setRefreshToken, getRefreshTokenFromCookie, getAccessToken, getHeader, removeToken} from "./Jwt";
+import React, { useState } from 'react'
+import axios from "axios";
+
+const BASE_URL = 'https://i8a107.p.ssafy.io'
+// const BASE_URL = 'http://localhost:8080'
+
+// test@gmail.com
+// 1234
+
+const LoginTest = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = async (event) => {
+    event.preventDefault();
+    console.log(`${BASE_URL}/api/user/login`)
+
+    try {
+      const response = await axios.post(`${BASE_URL}/api/user/login`, {
+        email,
+        password,
+      });
+
+      // Handle successful login
+      setIsLoggedIn(true);
+      console.log(response.data);
+      // console.log(response.data.refreshToken)
+      setRefreshToken(response.data.refreshToken)
+      console.log(getRefreshTokenFromCookie())
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  const logout = () => {
+    removeToken()
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <button type="button" onClick={logout}>
+          Logout
+        </button>
+      ) : (
+        <form onSubmit={login}>
+          <div>
+            <label htmlFor="email">email:</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <button type="submit">Login</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default LoginTest;
