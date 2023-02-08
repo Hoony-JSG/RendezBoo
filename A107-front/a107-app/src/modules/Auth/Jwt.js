f134b0089fea2fda348d98ef997bb7716294ebfd
Bluuubery <zephyr72836@gmail.com>
2023-02-08 15:21:40 +0900

    Feat: jwt 관련 모듈에 설명 추가

--- A107-front/a107-app/src/modules/Auth/Jwt.js
+++ A107-front/a107-app/src/modules/Auth/Jwt.js
@@ -7,18 +7,31 @@ import { Cookies } from "react-cookie";
 //   refreshToken: "dfdfdfdfdfdf"
 // }
 
+
+
+// refresh token / access token 유효시간 입니다.
 const REFRESH_TOKEN_AGE = 604800   // 1 week
 const ACCESS_TOKEN_AGE = 1800      // 30 min
 
-
+// 새로운 쿠키 객체 생성
 const cookies = new Cookies()
 
 
+// setRefreshToken: 발급 받은 Refresh token 쿠키에 저장
 export const setRefreshToken = (refreshToken) => {
-  // console.log(refreshToken)
+
   cookies.set(
-    'refresh_token',
-    refreshToken,
+    'refresh_token', // 이름
+    refreshToken, // 값
+    
+    // 보안 관련 설정 (에러 시 조정)
+    /**
+     * path: 모든 url에 대해 쿠키 전송 허용
+     * sameSite: 모든 도메인에서 쿠키에 접근 가능
+     * httpOnly: xss(자바스크립트를 통한 쿠키 탈취) 방지 옵션인데 도메인에서만 적용 가능(ip에서는 불가)
+     * sercure: https 통신만 허용 (sameSite: none 설정 위해 필요)
+     * maxAge: 쿠키 만료 기한 (일주일)
+     */
     {
       path: "/",
       sameSite: 'none',
@@ -27,32 +40,41 @@ export const setRefreshToken = (refreshToken) => {
       maxAge: REFRESH_TOKEN_AGE
     }
   )
-  // console.log(cookies.get(refreshToken.maxAge))
 }
 
 
+// getRefreshTokenFromCookie: 쿠키에 저장되어있는 refresh token 가져오기
 export const getRefreshTokenFromCookie = () => {
   return cookies.get('refresh_token')
 }
 
 
+// getAccessToken: Refresh token을 이용해 access token 재발급(reissue) 요청
 export const getAccessToken = async () => {
+
+  // 쿠키 가져오기
   const refreshToken = getRefreshTokenFromCookie()
 
+  // 쿠키가 없을 경우
   if (!refreshToken) {
-    console.log("토큰이 없습니다")
+    console.log("토큰이 없습니다") // 이후 적절히 예외처리 해주세요
     return
   }
 
+  // access 토큰 발급 axios 요청
   try {
     const response = await axios.post(
       'https:i8a107.p.ssafy.io/api/user/reissue',
+      // 헤더 부착
       {
         Authorization: `Bearer ${refreshToken}`
       }
     )
     
+    // 갱신 된 refresh token 저장
     setRefreshToken(response.data.refreshToken)
+
+    // access 토큰 반환
     return response.data.getAccessToken
 
   } catch (e) {
@@ -60,8 +82,9 @@ export const getAccessToken = async () => {
   }
 }
 
+
+// removeToken: (로그아웃 시) 쿠키에 저장된 토큰 제거
 export const removeToken = () => {
-  console.log('지워')
   cookies.remove('refresh_token',
   {
     path: "/",
@@ -72,6 +95,8 @@ export const removeToken = () => {
   })
 }
 
+
+// getHeader: 헤더에 토큰 부착해주기
 export const getHeader = (accessToekn) => {
   return {
     Authorization: `Bearer ${accessToekn}`
