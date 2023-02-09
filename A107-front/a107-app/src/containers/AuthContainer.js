d936ed11b3989350c30952953412476b2d0c247f
Bluuubery <zephyr72836@gmail.com>
2023-02-09 13:25:43 +0900

    Fix: auth container 수정
    initial state 분리
    기타 오탈자 수정

--- A107-front/a107-app/src/containers/AuthContainer.js
+++ A107-front/a107-app/src/containers/AuthContainer.js
@@ -2,22 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const ACCESS_TOKEN_AGE = 1800 * 1000    // 30 min
 
+const initialState = {
+  authenticated: false,
+  accessToken: null,
+  expireTime: null
+}
+
+
 export const accessTokenSlice = createSlice({
   name: 'accessToken',
-  initialState: {
-    authenticated: false,
-    accessToken: null,
-    expireTime: null
-  },
+  initialState: initialState,
   reducers: {
     SET_TOKEN: (state, action) => {
+      console.log(action)
       state.authenticated = true
       state.accessToken = action.payload
       state.expireTime = new Date().getTime() + ACCESS_TOKEN_AGE
     },
     REMOVE_TOKEN: (state) => {
       state.authenticated = false
-      state.accessToken = null,
+      state.accessToken = null
       state.expireTime = null
     }
   }
@@ -25,4 +29,4 @@ export const accessTokenSlice = createSlice({
 
 export const { SET_TOKEN, REMOVE_TOKEN } = accessTokenSlice.actions
 
-export default accessTokenSlice.reducer
+export const accessTokenReducer =  accessTokenSlice.reducer
