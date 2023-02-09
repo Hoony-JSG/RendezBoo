26167ad537291d962d3033b6be009e3741dc5892
Bluuubery <zephyr72836@gmail.com>
2023-02-09 16:41:01 +0900

    Feat: 로그아웃 페이지 작업

---
+++ A107-front/a107-app/src/pages/Logout.jsx
@@ -0,0 +1,55 @@
+import { removeToken } from '../modules/Auth/Jwt'
+import { REMOVE_TOKEN } from '../containers/JwtContainer'
+import { REMOVE_USER_INFO } from '../containers/UserInfo'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import axios from 'axios'
+
+// const BASE_URL = 'http://localhost:8080'
+
+// test@gmail.com
+// 1234
+
+const Logout = () => {
+  const BASE_URL = 'https://i8a107.p.ssafy.io'
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const accessToken = useSelector(
+    (state) => state.accessTokenReducer.accessToken
+  )
+
+  const logout = async () => {
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/api/user/logout`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+
+      removeToken()
+      dispatch(REMOVE_TOKEN())
+      dispatch(REMOVE_USER_INFO())
+
+      navigate('/home')
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  )
+}
+
+export default Logout
