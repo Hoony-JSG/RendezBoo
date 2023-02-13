17ed71a28a1a13cf853430f220ed1e2847c42802
developerhongjulee <developerhongjulee@gmail.com>
2023-02-13 16:01:58 +0900

    chore: 테스트용 merge

--- A107-front/a107-app/src/components/Docking3WaitingMeeting.js
+++ A107-front/a107-app/src/components/Docking3WaitingMeeting.js
@@ -379,6 +379,7 @@ const Docking3WaitingMeeting = ({ multiMeetingRoomSeq }) => {
                     startFaceAPI={() => {}}
                   />
                 </div>
+                
               ))}
               {publisher !== undefined ? (
                 <div className="cam">
