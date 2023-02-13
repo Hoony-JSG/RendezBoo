4e7c556c2e93dc575e628bb441d861bfcde6b5df
developerhongjulee <developerhongjulee@gmail.com>
2023-02-13 18:04:02 +0900

    feat: 멀티미팅방 나갔을때 0/0면 방 자동으로 삭제 기능 완료

--- A107-front/a107-app/src/components/Docking3WaitingMeeting.js
+++ A107-front/a107-app/src/components/Docking3WaitingMeeting.js
@@ -38,7 +38,7 @@ const Docking3WaitingMeeting = ({ multiMeetingRoomSeq }) => {
     CLOUD_FRONT_URL + 'images/glass-1-mask-1.png'
   )
 
-  useEffect(() => {
+  useEffect(() => { 
     connect()
     console.log('나를 이 미팅방-유저 테이블에 추가합니다.')
     axios
@@ -129,6 +129,19 @@ const Docking3WaitingMeeting = ({ multiMeetingRoomSeq }) => {
       })
 
     return () => disconnect()
+  }, [])
+
+  //뒤로가기 버튼 처리
+  const onBackButtonEvent = (e) => {
+      e.preventDefault();
+      navigate('/docking3')
+  }
+  useEffect(()=>{
+    window.history.pushState(null, null, window.location.pathname)
+    window.addEventListener('popstate', onBackButtonEvent)
+    return()=>{
+      window.removeEventListener('popstate', onBackButtonEvent)
+    }
   }, [])
 
   //////////////////////////////////////////////////////////////////////////////////////
@@ -228,11 +241,11 @@ const Docking3WaitingMeeting = ({ multiMeetingRoomSeq }) => {
     setMessage('')
   }
 
-  const disconnect = () => {
+  const disconnect = async () => {
     console.log('disconnect(): 대기방 연결을 해제합니다.')
     client.current.deactivate()
     console.log('나를 이 미팅방-유저 테이블에서 삭제합니다.')
-    axios
+    await axios
       .delete(
         APPLICATION_SERVER_URL +
           'api/multi-meetings/' +
@@ -240,9 +253,8 @@ const Docking3WaitingMeeting = ({ multiMeetingRoomSeq }) => {
           '/' +
           userSeq
       )
-      .then((response) => {
-        console.log(response.status)
-      })
+    navigate('/docking3')
+
   }
 
   // handleChage: 채팅 입력 시 state에 값 설정
