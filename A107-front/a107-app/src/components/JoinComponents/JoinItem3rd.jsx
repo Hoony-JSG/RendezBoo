f9db1de03ab95ff1306c8ec5afa6cd320bd8908a
wjdgnsxhsl <wjdgnsxhsl@naver.com>
2023-02-13 12:42:36 +0900

    feat: Join3 css 적용 중

--- A107-front/a107-app/src/components/JoinComponents/JoinItem3rd.jsx
+++ A107-front/a107-app/src/components/JoinComponents/JoinItem3rd.jsx
@@ -1,28 +1,51 @@
 import RocketItem from '../RocketComponents/RocketItem'
 import JoinItemBirth from './JoinItemBirth'
 import '../../Styles/temp_border_style.css'
+import '../../Styles/JoinItem3Style.css'
 import NextPageButton from './NextPageButton'
 import { useState } from 'react'
+
 const JoinItem3rd = (props) => {
   const [hasProfileImage, setHasProfileImage] = useState(false)
-  const [hasBirth, setHasBirth] = useState(true)
+  const [hasBirth, setHasBirth] = useState(false)
+  const wholeHas = [
+    [hasProfileImage, setHasProfileImage],
+    [hasBirth, setHasBirth],
+  ]
+
+  const setWholeHas = (index, value) => {
+    wholeHas[index][1](value)
+  }
+
   return (
-    <div>
-      <div className="temp_border_style">
-        <br />
-        J_3_로켓아이템 재활용
-        <div>
+    <div className="Join3_display">
+      <div className="Join3_whole-block">
+        <div
+          className={
+            'Join3_left-box ' + (hasProfileImage ? 'Join3_border-glow' : '')
+          }
+        >
           <RocketItem ver="Start" setTrue={setHasProfileImage} />
         </div>
+        <div className="Join3_right-box">
+          <div
+            className={
+              'Join3_right-top-box ' + (hasBirth ? 'Join3_border-glow' : '')
+            }
+          >
+            <JoinItemBirth setHas={setHasBirth} />
+          </div>
+          <div className="Join3_right-bottom-box">
+            <div className="Join3_next-button">
+              <NextPageButton
+                hasProfileImage={hasProfileImage}
+                hasBirth={hasBirth}
+                setNext={props.setNext}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="temp_border_style">
-        <JoinItemBirth setHas={setHasBirth} />
-      </div>
-      <NextPageButton
-        hasProfileImage={hasProfileImage}
-        hasBirth={hasBirth}
-        setNext={props.setNext}
-      />
     </div>
   )
 }
