c47c6802dcb5be3390f324c95f1b27f3f2f2b96e
Bluuubery <zephyr72836@gmail.com>
2023-02-14 14:33:02 +0900

    Fix: useEffect 함수 호출 에러 수정

--- A107-front/a107-app/src/App.js
+++ A107-front/a107-app/src/App.js
@@ -47,19 +47,21 @@ function App() {
   ]
   const isAllowedPath = allowedPaths.includes(location.pathname)
 
-  const reissueAndSetToken = async () => {
-    // 토큰이 없고 접근 제한된 라우트에 접근하려고 할 때 홈으로 보내기
-    if (!isAllowedPath && !refreshToken) {
-      alert('로그인이 필요한 서비스입니다.')
-      navigate('/home')
-      //  그 외에는 토큰 재발급
-    } else {
-      const accessToken = await reissueAccessToken(refreshToken)
-      dispatch(SET_TOKEN(accessToken))
+  useEffect(() => {
+    const reissueAndSetToken = async () => {
+      // 토큰이 없고 접근 제한된 라우트에 접근하려고 할 때 홈으로 보내기
+      if (!isAllowedPath && !refreshToken) {
+        alert('로그인이 필요한 서비스입니다.')
+        navigate('/home')
+        //  그 외에는 토큰 재발급
+      } else {
+        const accessToken = await reissueAccessToken(refreshToken)
+        dispatch(SET_TOKEN(accessToken))
+      }
     }
-  }
 
-  useEffect(reissueAndSetToken(), [location.pathname, refreshToken])
+    reissueAndSetToken()
+  }, [isAllowedPath, location.pathname, refreshToken, dispatch, navigate])
 
   return (
     <div className="App">
