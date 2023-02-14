6d7885f6f16be825c668f1710d4c31a74e2e8119
unknown <hi_cookie@nate.com>
2023-02-14 17:55:00 +0900

    fix: signal timestamp 수정

--- A107-front/a107-app/src/components/SignalComponents/SignalSelectedItem.jsx
+++ A107-front/a107-app/src/components/SignalComponents/SignalSelectedItem.jsx
@@ -3,7 +3,10 @@ import { useSelector } from 'react-redux'
 const SignalListItem = ({ chat, you }) => {
   const { message, createdAt, senderSeq } = chat
   const me = useSelector((state) => state.userInfoReducer.userSeq)
-  const time = typeof(createdAt) === "string" ? new Date(createdAt) : new Date(...createdAt)
+  const time =
+    typeof createdAt === 'string'
+      ? new Date(createdAt)
+      : new Date(...createdAt.slice(0, 5))
 
   return (
     <div>
@@ -19,16 +22,28 @@ const SignalListItem = ({ chat, you }) => {
           <p className="your-name">{you.yourname}</p>
           <div className="your-signal">
             <p>{message}</p>
-            <p id="time">
-              {time.getHours()}:{time.getMinutes()}
-            </p>
+            {parseInt(time.getHours() / 12) ? (
+              <p id="time">
+                {time.getHours() % 12 || 12}:{time.getMinutes()} PM
+              </p>
+            ) : (
+              <p id="time">
+                {time.getHours()}:{time.getMinutes()} AM
+              </p>
+            )}
           </div>
         </div>
       ) : (
         <div className="my-signal">
-          <p id="time">
-            {time.getHours()}:{time.getMinutes()}
-          </p>
+          {parseInt(time.getHours() / 12) ? (
+            <p id="time">
+              {time.getHours() % 12 || 12}:{time.getMinutes()} PM
+            </p>
+          ) : (
+            <p id="time">
+              {time.getHours() || 12}:{time.getMinutes()} AM
+            </p>
+          )}
           <p>{message}</p>
         </div>
       )}
