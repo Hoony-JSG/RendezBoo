4fbe5fe1cf5473d3e7c03470cbac17794508825b
wjdgnsxhsl <wjdgnsxhsl@naver.com>
2023-02-14 12:18:59 +0900

    feat: MBTI 디자인 적용 준비

--- A107-front/a107-app/src/components/RocketComponents/RocketItem.jsx
+++ A107-front/a107-app/src/components/RocketComponents/RocketItem.jsx
@@ -31,7 +31,9 @@ const RocketItem = (props) => {
   console.log('Btn :' + ver)
   return (
     <div style={setTempBorder}>
-      <div className="Join3_profileimage">프로필 이미지 업로드</div>
+      {props.ver === 'Start' && (
+        <div className="Join3_profileimage">About Me</div>
+      )}
       <div className="Join3_uploader">
         <div className="Join3_uiverse">
           <div className="Join3_uiverse-image-card"></div>
@@ -48,8 +50,6 @@ const RocketItem = (props) => {
           </div>
         )}
       </div>
-      <h3>Me = {Me}</h3>
-      <h5>Inquire = {Inquire}</h5>
 
       <RocketBtn mbti={props.mbti} {...MeAndYou} />
     </div>
