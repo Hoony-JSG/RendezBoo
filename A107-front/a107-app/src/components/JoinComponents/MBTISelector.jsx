da1c3e98d11f7f5ebbd1f013132781f886abac0a
wjdgnsxhsl <wjdgnsxhsl@naver.com>
2023-02-15 14:51:38 +0900

    feat: myrocket 좌측 카드 생성

--- A107-front/a107-app/src/components/JoinComponents/MBTISelector.jsx
+++ A107-front/a107-app/src/components/JoinComponents/MBTISelector.jsx
@@ -21,58 +21,61 @@ const MBTISelector = (props) => {
   useEffect(updateMBTI, [IE, SN, TF, JP])
 
   return (
-    <div className="mbti-selector">
-      <div className="MBTISelector_head">
-        <div className="MBTISelector_text">MBTI Selector</div>
-      </div>
-      <div className="mbti-selector-item">
-        <div className="MBTISelector_btn">
+    <div
+      className={`${
+        props.setBtn === !undefined ? 'mbti-selector-edit' : 'mbti-selector'
+      }`}
+    >
+      {props.setBtn !== undefined ? (
+        ''
+      ) : (
+        <div className="MBTISelector_head">
+          <div className="MBTISelector_text">MBTI Selector</div>
+        </div>
+      )}
+      <div
+        className={
+          props.setBtn === !undefined
+            ? 'mbti-selector-item-edit'
+            : 'mbti-selector-item'
+        }
+      >
+        <div
+          className={
+            props.setBtn === !undefined
+              ? 'MBTISelector_btn-edit'
+              : 'MBTISelector_btn'
+          }
+        >
           <MBTIBtn changeData={setIE} data1="I" data2="E" />
         </div>
-        <div className="MBTISelector_btn">
+        <div
+          className={
+            props.setBtn === !undefined
+              ? 'MBTISelector_btn-edit'
+              : 'MBTISelector_btn'
+          }
+        >
           <MBTIBtn changeData={setSN} data1="S" data2="N" />
         </div>
-        <div className="MBTISelector_btn">
+        <div
+          className={
+            props.setBtn === !undefined
+              ? 'MBTISelector_btn-edit'
+              : 'MBTISelector_btn'
+          }
+        >
           <MBTIBtn changeData={setTF} data1="T" data2="F" />
         </div>
-        <div className="MBTISelector_btn">
+        <div
+          className={
+            props.setBtn === !undefined
+              ? 'MBTISelector_btn-edit'
+              : 'MBTISelector_btn'
+          }
+        >
           <MBTIBtn changeData={setJP} data1="J" data2="P" />
         </div>
-        {/* 
-        <div>I/E</div>
-        <button
-          className="mbti-selector-item-button"
-          onClick={() => updateMBTI('IE')}
-        >
-          <div className="mbti-selector-item-div">{MBTI.I}</div>
-        </button>
-      </div>
-      <div className="mbti-selector-item">
-        <div>S/N</div>
-        <button
-          className="mbti-selector-item-button"
-          onClick={() => updateMBTI('SN')}
-        >
-          <div className="mbti-selector-item-div">{MBTI.S}</div>
-        </button>
-      </div>
-      <div className="mbti-selector-item">
-        <div>F/T</div>
-        <button
-          className="mbti-selector-item-button"
-          onClick={() => updateMBTI('TF')}
-        >
-          <div className="mbti-selector-item-div">{MBTI.F}</div>
-        </button>
-      </div>
-      <div className="mbti-selector-item">
-        <div>J/P</div>
-        <button
-          className="mbti-selector-item-button"
-          onClick={() => updateMBTI('PJ')}
-        >
-          <div className="mbti-selector-item-div">{MBTI.J}</div>
-        </button> */}
       </div>
     </div>
   )
