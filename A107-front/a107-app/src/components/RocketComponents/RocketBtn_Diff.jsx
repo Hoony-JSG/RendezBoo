e81c4e211edf73f5d113d301e648193148d767f8
wjdgnsxhsl <wjdgnsxhsl@naver.com>
2023-02-16 15:36:30 +0900

    feat: Other Rocket 완성

--- A107-front/a107-app/src/components/RocketComponents/RocketBtn_Diff.jsx
+++ A107-front/a107-app/src/components/RocketComponents/RocketBtn_Diff.jsx
@@ -1,29 +1,15 @@
 import { useNavigate } from 'react-router-dom'
 import '../../Styles/RocketItemButtonStyle.css'
+import rocketImg from '../../Images/RendezRocket.png'
 const RocketBtn_Diff = (props) => {
   const navigate = useNavigate()
-  function doSignal() {
-    console.log('메세지창')
-    navigate('/signal')
-  }
-  function deleteFriend() {
-    console.log('친구 삭제')
-  }
-  function report() {
-    console.log('신고하기')
-    navigate('/report')
-  }
   return (
-    <div>
-      <button className="RocketItemButton" onClick={doSignal}>
-        Message
-      </button>
-      <button className="RocketItemButton" onClick={deleteFriend}>
-        친구 삭제
-      </button>
-      <button className="RocketItemButton" onClick={report}>
-        신고하기
-      </button>
+    <div className="RocketBtnDiff_container">
+      <img
+        className="RocketBtnDiff_rocket-img"
+        src={rocketImg}
+        alt="Rocket"
+      ></img>
     </div>
   )
 }
